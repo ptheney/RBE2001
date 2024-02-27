cfbe5f1abad6aae325326ed14db26ff430539ed4
ptheney <ptheney@wpi.edu>
2024-02-26 22:05:59 -0500

    test

---
+++ src/main.cpp
@@ -0,0 +1,135 @@
+#include <Arduino.h>
+#include <Romi32U4.h>
+#include <Romi32U4Motors.h>
+#include <Chassis.h>
+#include <IRdecoder.h>
+#include <ir_codes.h>
+
+// Define constants for states
+#define IDLE   0
+#define TASK_1 1
+#define TASK_2 2
+#define TASK_3 3
+#define TASK_4 4
+#define TASK_5 5
+
+// Create objects
+Chassis chassis;
+
+int motorEffort = 100;
+
+// Starting state is idle
+int state = IDLE;
+
+// Set up IR decoder
+const uint8_t IR_DETECTOR_PIN = 14;
+IRDecoder decoder(IR_DETECTOR_PIN);
+int16_t keyPress;
+
+void setup()
+{
+  Serial.begin(9600);
+
+  // Initialize chassis, and IR decoder
+  chassis.init(); 
+  decoder.init();
+}
+
+// Handles key press from IR remote
+void handleKeyPress(int16_t keyPress)
+{
+  // Print key that was pressed
+  Serial.println("Key: " + String(keyPress));
+
+  // When stop is pressed, go to idle unless already in idle
+  if (keyPress == STOP_MODE)
+  {
+    // If not already idle, switch to idle
+    if (state != IDLE)
+      state = IDLE;
+    // If already idle, then must be in middle of Task 3
+    else
+      state = TASK_3;
+  }
+     
+  // When number 1 is pressed, execute task 1
+  if (keyPress == NUM_1)
+     state = TASK_1;
+
+  // When number 2 is pressed, execute task 2
+  if (keyPress == NUM_2)
+    state = TASK_2;
+
+  // When number 3 is pressed, execute task 3
+  if (keyPress == NUM_3)
+    state = TASK_3;  
+
+  // When number 4 is pressed, execute task 4
+  if (keyPress == NUM_4)
+    state = TASK_4;   
+
+  // When number 5 is pressed, execute task 5
+  if (keyPress == NUM_5)
+    state = TASK_5; 
+}
+
+void loop()
+{
+  // Check for a key press on the remote
+  keyPress = decoder.getKeyCode();
+  
+  // Handle key press on IR remote
+  if (keyPress > -1) 
+    handleKeyPress(keyPress); 
+
+  // State 0: IDLE
+  if (state == IDLE)
+  {
+    chassis.idle();
+  }
+  // State 1: Run Task 1
+  else if (state == TASK_1)
+  {
+    // Drive to the house
+    // Pick panel
+    // Rotate 180 degrees
+    // Place panel at staging area
+    // Stop
+  }
+  // State 2: Task 2
+  else if (state == TASK_2)
+  {
+    // Drive towards the staging area
+    // Pick panel
+    // Rotate 180 degrees
+    // Place panel at house
+    // Release panel
+    // Stop
+  }
+  // State 3: Task 3
+  else if (state == TASK_3)
+  {
+    // Use line following algorithm to navigate to other house
+    // Stop robot halfway (switch state to idle)
+    // Continue line following
+  }
+  // State 4: Task 4
+  else if (state == TASK_4)
+  {
+    // Drive to the house
+    // Pick panel 
+    // Rotate 180 degrees
+    // Place panel at staging area
+    // Stop
+  }
+  // State 5: Task 5
+  else if (state == TASK_5)
+  {
+    // Drive towards the staging area
+    // Pick panel
+    // Rotate 180 degrees
+    // Place panel at house
+    // Release panel
+    // Stop
+  }
+}
